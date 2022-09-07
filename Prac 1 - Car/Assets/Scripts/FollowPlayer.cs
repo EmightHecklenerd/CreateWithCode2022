301d38b76f91648b1cf8d2dfebba374876bdf9b2
Emight <ehenkener@gmail.com>
2022-09-07 12:22:59 -0400

    fixing more

--- Prac 1 - Car/Assets/Scripts/FollowPlayer.cs
+++ Prac 1 - Car/Assets/Scripts/FollowPlayer.cs
@@ -3,16 +3,21 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class FollowPlayer : MonoBehaviour
+
 {
+
     // Start is called before the first frame update
     void Start()
     {
         
     }
 
+
+    public GameObject player;
+    private Vector3 offset = new Vector3(0, 5, -7);
     // Update is called once per frame
-    void Update()
+    void LateUpdate()
     {
-        
+        transform.position = player.transform.position; + offset;
     }
 }
