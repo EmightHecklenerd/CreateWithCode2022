d1cdf0ff7fd6daee5f2c67b28cdfbebd68e0ff72
Emight <ehenkener@gmail.com>
2022-09-09 14:33:44 -0400

    final plane commit

--- Prac 1 Challenge Plane/Assets/Challenge 1/Scripts/FollowPlayerX.cs
+++ Prac 1 Challenge Plane/Assets/Challenge 1/Scripts/FollowPlayerX.cs
@@ -5,8 +5,8 @@ using UnityEngine;
 public class FollowPlayerX : MonoBehaviour
 {
     public GameObject player;
-    private Vector3 offset = new Vector3(55.40, 2.28, 4.48);
-
+    private Vector3 offset1 = new Vector3(55.40f, 2.28f, 4.48f);
+    
     // Start is called before the first frame update
     void Start()
     {
@@ -16,7 +16,7 @@ public class FollowPlayerX : MonoBehaviour
     // Update is called once per frame
     void LateUpdate()
     {
-        transform.position = player.transform.position + offset;
-        transform.rotation = player.transform.rotation + new Vector3(0, 270.524, 0);
+        transform.position = player.transform.position + offset1;
+        
     }
 }
