f327b5d53c09c8a770b3f1deb1cc2ed4e9237a8d
Emight <ehenkener@gmail.com>
2022-10-04 11:26:45 -0400

    it do go down

--- Challenge 3 - Starter Files/Assets/Challenge 3/Scripts/PlayerControllerX.cs
+++ Challenge 3 - Starter Files/Assets/Challenge 3/Scripts/PlayerControllerX.cs
@@ -6,7 +6,7 @@ public class PlayerControllerX : MonoBehaviour
 {
     public bool gameOver = false;
 
-    public float floatForce;
+    public float floatForce = 15f;
     private float gravityModifier = 1.5f;
     private Rigidbody playerRb;
 
@@ -23,7 +23,7 @@ public class PlayerControllerX : MonoBehaviour
     {
         Physics.gravity *= gravityModifier;
         playerAudio = GameObject.Find("Player").GetComponent<AudioSource>();
-
+        playerRb = GetComponent<Rigidbody>();
         // Apply a small upward force at the start of the game
         playerRb.AddForce(Vector3.up * 5, ForceMode.Impulse);
 
@@ -35,7 +35,7 @@ public class PlayerControllerX : MonoBehaviour
         // While space is pressed and player is low enough, float up
         if (Input.GetKeyDown(KeyCode.Space) && !gameOver)
         {
-            playerRb.AddForce(Vector3.up *  floatForce);
+            playerRb.AddForce(Vector3.up * floatForce);
         }
     }
 
