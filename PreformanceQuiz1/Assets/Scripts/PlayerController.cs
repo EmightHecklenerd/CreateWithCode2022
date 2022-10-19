353845bf2b4112e57a97b86af154bdf345e25bee
Emight <ehenkener@gmail.com>
2022-10-19 11:29:37 -0400

    preformance quiz 1

---
+++ PreformanceQuiz1/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed = 10f;
+    public float rotationSpeed = 100f;
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("Game Started");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float translation = Input.GetAxis("Vertical") * speed;
+        float rotation = Input.GetAxis("Horizontal") * rotationSpeed;
+    }
+}
