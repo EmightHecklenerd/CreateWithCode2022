80d58a0699fdf0bfda6ef4d8e0f9665c6a2da042
Emight <ehenkener@gmail.com>
2022-11-15 12:28:01 -0500

    speeddd

--- Proto 5/Assets/Scripts/Target.cs
+++ Proto 5/Assets/Scripts/Target.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class Target : MonoBehaviour
 {
 
-    private float minSpeed = 12;
+    private float minSpeed = 10;
     private float maxSpeed = 14;
     private float maxTorque = 10;
     private float xRange = 4;
