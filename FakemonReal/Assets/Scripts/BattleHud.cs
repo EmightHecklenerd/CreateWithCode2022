d6561d8237300e69a45d1b0a297d66bc5ac938ae
Emight <ehenkener@gmail.com>
2023-01-04 12:18:38 -0500

    codes

---
+++ FakemonReal/Assets/Scripts/BattleHud.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BattleHud : MonoBehaviour
+{
+
+    public Text NameText;
+    public Text levelText;
+    public Text HPText;
+    public Slider HealthBar; 
+
+    public void SetHUD()
+    {
+
+    }
+
+
+
+
+
+}
